0ae6d1b7b22cfb6234b56de5b20c5723929ec4c1
Arkadii Lantukh <lan18029@byui.edu>
2022-06-16 16:29:01 +0300

    Dynamic Product List | Individual 3 [end of week 2]

--- build/js/utils.js
+++ build/js/utils.js
@@ -1,1 +1,20 @@
-export function qs(t){return document.querySelector(t)}export function getLocalStorage(t){return JSON.parse(localStorage.getItem(t))}export function setLocalStorage(t,e){localStorage.setItem(t,JSON.stringify(e))}export function setClick(t,e){qs(t).addEventListener("touchend",r=>{r.preventDefault(),e()}),qs(t).addEventListener("click",e)}export function getParam(t){const e=window.location.search,r=new URLSearchParams(e);return r.get(t)}
+export function qs(t) {
+  return document.querySelector(t);
+}
+export function getLocalStorage(t) {
+  return JSON.parse(localStorage.getItem(t));
+}
+export function setLocalStorage(t, e) {
+  localStorage.setItem(t, JSON.stringify(e));
+}
+export function setClick(t, e) {
+  qs(t).addEventListener("touchend", (r) => {
+    r.preventDefault(), e();
+  }),
+    qs(t).addEventListener("click", e);
+}
+export function getParam(t) {
+  const e = window.location.search,
+    r = new URLSearchParams(e);
+  return r.get(t);
+}
