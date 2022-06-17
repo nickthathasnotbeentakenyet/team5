b6e1351a4bde42b0d61008a4b9c540950d2d3366
Denis5108 <gar18056@byui,edu>
2022-06-17 15:34:25 -0600

    main.js files edited

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@ import productData from "./productData.js";
 import productList from "./productList.js";
 
 // first create an instance of our ProductData class.
-const dataSource = new ProductData("tents");
+const dataSource = new productData("tents");
 // then get the element we want the product list to render in
 const listElement = document.querySelector(".product-list");
 // then create an instance of our ProductList class and send it the correct information.
-const myList = new ProductList("tents", dataSource, listElement);
+const myList = new productList("tents", dataSource, listElement);
 // finally call the init method to show our products
 myList.init();
