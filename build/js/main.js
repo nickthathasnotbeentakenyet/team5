6d974017a1f47bfa5f2d7d1ef060696efaaeee7d
Arkadii Lantukh <lan18029@byui.edu>
2022-06-23 07:50:57 +0300

    week 4

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,6 @@
-import t from"./productData.js";import o from"./productList.js";const r=new t("tents"),s=document.querySelector(".product-list"),c=new o("tents",r,s);c.init();
+import t from "./productData.js";
+import o from "./productList.js";
+const r = new t("tents"),
+  s = document.querySelector(".product-list"),
+  c = new o("tents", r, s);
+c.init();
--- build/js/utils.js
+++ build/js/utils.js
@@ -1,1 +1,27 @@
-export function qs(t){return document.querySelector(t)}export function getLocalStorage(t){return JSON.parse(localStorage.getItem(t))}export function setLocalStorage(t,e){localStorage.setItem(t,JSON.stringify(e))}export function setClick(t,e){qs(t).addEventListener("touchend",n=>{n.preventDefault(),e()}),qs(t).addEventListener("click",e)}export function getParam(t){const e=window.location.search,n=new URLSearchParams(e);return n.get(t)}export function renderListWithTemplate(t,e,n,o){n.forEach(r=>{const a=t.content.cloneNode(!0),c=o(a,r);e.appendChild(c)})}
+export function qs(t) {
+  return document.querySelector(t);
+}
+export function getLocalStorage(t) {
+  return JSON.parse(localStorage.getItem(t));
+}
+export function setLocalStorage(t, e) {
+  localStorage.setItem(t, JSON.stringify(e));
+}
+export function setClick(t, e) {
+  qs(t).addEventListener("touchend", (n) => {
+    n.preventDefault(), e();
+  }),
+    qs(t).addEventListener("click", e);
+}
+export function getParam(t) {
+  const e = window.location.search,
+    n = new URLSearchParams(e);
+  return n.get(t);
+}
+export function renderListWithTemplate(t, e, n, o) {
+  n.forEach((r) => {
+    const a = t.content.cloneNode(!0),
+      c = o(a, r);
+    e.appendChild(c);
+  });
+}
