295a2603a6c10eff9803c707b6a9c1d1d050e3a2
Arkadii Lantukh <lan18029@byui.edu>
2022-07-01 21:13:41 +0300

    team7 | still having an error for card number

--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,4 +1,9 @@
-import { setLocalStorage, getLocalStorage, loadHeaderFooter } from "./utils.js";
+import {
+  setLocalStorage,
+  getLocalStorage,
+  loadHeaderFooter,
+  alertMessage,
+} from "./utils.js";
 
 loadHeaderFooter();
 export default class ProductDetails {
@@ -25,6 +30,7 @@ export default class ProductDetails {
     // then add the current product to the list
     cartContents.push(this.product);
     setLocalStorage("so-cart", cartContents);
+    alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
   }
   renderProductDetails() {
     return `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
